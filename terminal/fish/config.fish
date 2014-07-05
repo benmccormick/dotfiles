6add4e75f31da261b0253f4cee47cd4ee3959cce
Ben McCormick <ben.mccormick@windsorcircle.com>
2014-07-05 00:23:50 -0400

    Catching up on dotfile changes to commit

--- terminal/fish/config.fish
+++ terminal/fish/config.fish
@@ -13,7 +13,7 @@ set PATH ~/Code/Windsor/test/bin /Library/PostgreSQL/9.3/bin $PATH
 set fish_greeting (whoami):(date +"%r")
 
 # Set Base16 Shell colors
-#eval sh ~/.config/fish/base16-eighties.dark.sh
+eval sh ~/.config/fish/base16-eighties.dark.sh
 if test -f /Users/ben/.autojump/etc/profile.d/autojump.fish; . /Users/ben/.autojump/etc/profile.d/autojump.fish; end
 
 
---
+++ vim/vim.symlink/ftplugin/mkd.vim
@@ -0,0 +1,5 @@
+"No Autocomplete for markdown
+let g:ycm_auto_trigger = 0
+"Wrap lines for markdown
+set wrap
+set linebreak                         " Wrap lines at convenient points for markdown only

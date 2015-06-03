fac9e3abdb1775861932e292f57ce6960d89b31f
Ben McCormick <ben.mccormick@windsorcircle.com>
2015-06-02 22:47:55 -0400

    dotfile updates - I don't commit often enough to be specific unfortunately

--- terminal/zsh.symlink/exports.zsh
+++ terminal/zsh.symlink/exports.zsh
@@ -1,10 +1,15 @@
 # setup PATH
-export PATH=$HOME/bin:/usr/local/bin:$PATH
+export PATH=$HOME/bin:/usr/local/bin:$HOME/Code/Windsor/wcui_dev/bin:$HOME/Code/Windsor/scripts/:/Library/PostgreSQL/9.3/bin:/usr/local/go/bin:$PATH
+export PYTHONPATH=~/Code/experiments/personally_api/models:$PYTHONPATH
+export EDITOR='mvim -v'
+
 # set terminal to 256 colors
 [ -z "$TMUX" ] && export TERM=xterm-256color
+
 # Setup autojump
-[[ -s `brew --prefix`/etc/autojump.zsh ]] && . `brew --prefix`/etc/autojump.zsh
+#[[ -s `brew --prefix`/etc/autojump.zsh ]] && . `brew --prefix`/etc/autojump.zsh
+
 # Base16 Shell
-BASE16_SCHEME="eighties"
+BASE16_SCHEME="flat"
 BASE16_SHELL="$HOME/.config/base16-shell/base16-$BASE16_SCHEME.dark.sh"
 [[ -s $BASE16_SHELL ]] && . $BASE16_SHELL

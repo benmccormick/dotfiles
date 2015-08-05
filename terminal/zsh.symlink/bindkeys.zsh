9ffa2155764c29f5d549013a1bdbecc31ea104c2
Ben McCormick <ben@benm-wc.local>
2015-08-05 16:15:34 -0400

    Update to dotfiles

--- terminal/zsh.symlink/bindkeys.zsh
+++ terminal/zsh.symlink/bindkeys.zsh
@@ -7,4 +7,10 @@ bindkey -v
 bindkey "^?" backward-delete-char
 # Accept auto suggestions
 bindkey '^f' vi-forward-blank-word
+# Search backwards and forwards with a pattern
+bindkey -M vicmd '/' history-incremental-search-backward
+bindkey -M vicmd '?' history-incremental-search-forward
 
+# set up for insert mode too
+bindkey -M viins '^R' history-incremental-search-backward
+bindkey -M viins '^F' history-incremental-search-forward
